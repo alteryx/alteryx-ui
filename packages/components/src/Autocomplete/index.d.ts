61ce5bc8c5b63b4ebc9df09202d9da1effd8bd5c
jordan-quinn-alteryx <jordan.quinn@alteryx.com>
2021-05-27 19:19:13 +0000

    Publish changes

--- packages/components/src/Autocomplete/index.d.ts
+++ packages/components/src/Autocomplete/index.d.ts
@@ -1,1 +1,162 @@
-export { default, createFilterOptions } from '@material-ui/lab/Autocomplete';
+/* eslint-disable @typescript-eslint/naming-convention */
+import React from 'react';
+import {
+  createFilterOptions,
+  AutocompleteClassKey,
+  AutocompleteRenderInputParams,
+  AutocompleteRenderGroupParams,
+  AutocompleteRenderOptionState,
+  AutocompleteGetTagProps
+} from '@material-ui/lab/Autocomplete';
+import { PopperProps, StandardProps } from '@material-ui/core';
+
+import { UseAutocompleteProps } from '../useAutocomplete';
+import { IVirtualizedListProps } from '../VirtualizedList';
+
+interface AutocompleteProps<
+  T,
+  Multiple extends boolean | undefined,
+  DisableClearable extends boolean | undefined,
+  FreeSolo extends boolean | undefined
+>
+  extends UseAutocompleteProps<T, Multiple, DisableClearable, FreeSolo>,
+    StandardProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      AutocompleteClassKey,
+      'defaultValue' | 'onChange' | 'children'
+    > {
+  /**
+   * Props applied to the [`Chip`](/api/chip/) element.
+   */
+  ChipProps?: object;
+  /**
+   * The icon to display in place of the default close icon.
+   */
+  closeIcon?: React.ReactNode;
+  /**
+   * Override the default text for the *clear* icon button.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   */
+  clearText?: string;
+  /**
+   * Override the default text for the *close popup* icon button.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   */
+  closeText?: string;
+  /**
+   * If `true`, the input will be disabled.
+   */
+  disabled?: boolean;
+  /**
+   * Disable the portal behavior.
+   * The children stay within it's parent DOM hierarchy.
+   */
+  disablePortal?: boolean;
+  /**
+   * Force the visibility display of the popup icon.
+   */
+  forcePopupIcon?: true | false | 'auto';
+  /**
+   * If `true`, the input will take up the full width of its container.
+   */
+  fullWidth?: boolean;
+  /**
+   * The label to display when the tags are truncated (`limitTags`).
+   *
+   * @param {number} more The number of truncated tags.
+   * @returns {ReactNode}
+   */
+  getLimitTagsText?: (more: number) => React.ReactNode;
+  /**
+   * The component used to render the listbox.
+   */
+  ListboxComponent?:
+    | React.ComponentType<React.HTMLAttributes<HTMLElement>>
+    | React.ComponentType<IVirtualizedListProps>;
+  /**
+   * Props applied to the Listbox element.
+   */
+  ListboxProps?: object;
+  /**
+   * If `true`, the component is in a loading state.
+   */
+  loading?: boolean;
+  /**
+   * Text to display when in a loading state.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   */
+  loadingText?: React.ReactNode;
+  /**
+   * The maximum number of tags that will be visible when not focused.
+   * Set `-1` to disable the limit.
+   */
+  limitTags?: number;
+  /**
+   * Text to display when there are no options.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   */
+  noOptionsText?: React.ReactNode;
+  /**
+   * Override the default text for the *open popup* icon button.
+   *
+   * For localization purposes, you can use the provided [translations](/guides/localization/).
+   */
+  openText?: string;
+  /**
+   * The component used to render the body of the popup.
+   */
+  PaperComponent?: React.ComponentType<React.HTMLAttributes<HTMLElement>>;
+  /**
+   * The component used to position the popup.
+   */
+  PopperComponent?: React.ComponentType<PopperProps>;
+  /**
+   * The icon to display in place of the default popup icon.
+   */
+  popupIcon?: React.ReactNode;
+  /**
+   * Render the group.
+   *
+   * @param {any} option The group to render.
+   * @returns {ReactNode}
+   */
+  renderGroup?: (params: AutocompleteRenderGroupParams) => React.ReactNode;
+  /**
+   * Render the input.
+   *
+   * @param {object} params
+   * @returns {ReactNode}
+   */
+  renderInput: (params: AutocompleteRenderInputParams) => React.ReactNode;
+  /**
+   * Render the option, use `getOptionLabel` by default.
+   *
+   * @param {T} option The option to render.
+   * @param {object} state The state of the component.
+   * @returns {ReactNode}
+   */
+  renderOption?: (option: T, state: AutocompleteRenderOptionState) => React.ReactNode;
+  /**
+   * Render the selected value.
+   *
+   * @param {T[]} value The `value` provided to the component.
+   * @param {function} getTagProps A tag props getter.
+   * @returns {ReactNode}
+   */
+  renderTags?: (value: T[], getTagProps: AutocompleteGetTagProps) => React.ReactNode;
+  /**
+   * The size of the autocomplete.
+   */
+  size?: 'small' | 'medium';
+}
+export default function Autocomplete<
+  T,
+  Multiple extends boolean | undefined = undefined,
+  DisableClearable extends boolean | undefined = undefined,
+  FreeSolo extends boolean | undefined = undefined
+>(props: AutocompleteProps<T, Multiple, DisableClearable, FreeSolo>): JSX.Element;
+export { createFilterOptions };
--- packages/components/src/Autocomplete/index.js
+++ packages/components/src/Autocomplete/index.js
@@ -1,5 +1,58 @@
-import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import MAutocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { dataUICMaker } from '../utils';
+
+const Autocomplete = props => {
+  const [prevIndex, setPrevIndex] = useState(null);
+  const [focused, setFocused] = useState(null);
+  const [direction, setDirection] = useState('');
+  const { ListboxComponent, disableListWrap, ListboxProps, onHighlightChange, ...rest } = props;
+
+  const handleOnHighlightChange = (e, value, reason) => {
+    const focusedEl = document.querySelector('[data-focus="true"]');
+    if (e && e.key) {
+      setDirection(e.key);
+    }
+    if (focusedEl !== null) setFocused(focusedEl);
+    if (typeof onHighlightChange === 'function') onHighlightChange(e, value, reason);
+  };
+
+  useEffect(() => {
+    if (focused !== null) {
+      const index = parseInt(focused.dataset.optionIndex, 10);
+      if (direction === 'ArrowDown' || direction === 'ArrowUp') {
+        setPrevIndex(index);
+      }
+    }
+  }, [focused, direction]);
+
+  return ListboxComponent.muiName === 'VirtualizedList' &&
+    (disableListWrap === false || disableListWrap === undefined) ? (
+    <MAutocomplete
+      {...rest}
+      ListboxComponent={ListboxComponent}
+      ListboxProps={{ ...ListboxProps, prevIndex, direction }}
+      onHighlightChange={handleOnHighlightChange}
+    />
+  ) : (
+    <MAutocomplete {...props} />
+  );
+};
+
+Autocomplete.propTypes = {
+  disableListWrap: PropTypes.bool,
+  ListboxComponent: PropTypes.elementType,
+  ListboxProps: PropTypes.objectOf(PropTypes.any),
+  onHighlightChange: PropTypes.func
+};
+
+Autocomplete.defaultProps = {
+  disableListWrap: undefined,
+  ListboxComponent: 'ul',
+  ListboxProps: { prevIndex: null, direction: '' },
+  onHighlightChange: undefined
+};
 
 export default dataUICMaker(Autocomplete, 'Autocomplete');
 export { createFilterOptions };
