e9446358c03f37e9af985590c27deacc805b45aa
Jordan Quinn <jordan.quinn@alteryx.com>
2021-08-04 14:05:37 -0600

    White space to test diff

--- packages/components/src/AccordionDetails/index.js
+++ packages/components/src/AccordionDetails/index.js
@@ -3,3 +3,7 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import { dataUICMaker } from '../utils';
 
 export default dataUICMaker(AccordionDetails, 'AccordionDetails');
+
+
+
+
